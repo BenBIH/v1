8335af07b267ebf78ee65b8f1234d7aec9daab28
Ben <benjo0103@hotmail.com>
2018-03-14 00:29:48 +0100

    Initial commit

---
+++ routes/contact.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var fs = require('fs');
+
+
+/* GET contact page. */
+router.get('/', function(req, res) {
+  res.sendfile('./views/contact.html');
+});
+
+/* POST contact page info */
+router.post('/', function(req, res) {
+    
+    var now = new Date(); 
+    
+    fs.appendFile( 'kontakt-' + now.getFullYear() + "-" + now.getMonth() + "-" + now.getDate() + "-" + now.getHours()  + '.json', JSON.stringify(req.body), function() {
+//        console.log('file writter');
+     res.sendfile('./views/hvala.html');
+
+    }); 
+
+})
+
+
+    
+module.exports = router;
---
+++ routes/login.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET login page. */
+router.get('/', function(req, res, next) {
+  res.sendfile('./views/login.html');
+
+
+});
+
+
+router.post('/', function(req, res) {
+
+    if (req.body.user == 'ben' && req.body.pass == '123') {
+        console.log('Logiram');
+        res.redirect('/success');
+    }
+    
+    else {
+        console.log('Vracam na pocetak');
+        res.sendfile('./views/login.html');
+    }
+    });
+
+module.exports = router;
