0dacec12f9dd0c7dbefe0d0d82b7d1dfd98a5b5d
Ben <benjo0103@hotmail.com>
2018-03-14 17:16:37 +0100

    Contact us .json month fix

--- routes/contact.js
+++ routes/contact.js
@@ -13,8 +13,8 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
     
     var now = new Date(); 
-    
-    fs.appendFile( 'kontakt-' + now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate() + "-" + now.getHours()  + '.json', JSON.stringify(req.body), function() {
+
+    fs.appendFile( 'kontakt-' + now.getFullYear() + "-" + parseInt(now.getMonth()+1) + "-" + now.getDate() + "-" + now.getHours()  + '.json', JSON.stringify(req.body), function() {
 //        console.log('file writter');
      res.sendfile('./views/hvala.html');
 
