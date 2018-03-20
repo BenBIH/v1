dcce45f41d78ddc661fa77eb81cbaf36882a1793
Ben <benjo0103@hotmail.com>
2018-03-20 19:49:50 +0100

    small change

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/users.js');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('users');
+router.get('/', function(req,res,next) {
+  User.find({}, function(err, data) {
+    if (!err) {
+      res.render('users', { title: 'Users', data: data});
+    } else {
+      throw err;
+    }
+  });
 });
 
 module.exports = router;
